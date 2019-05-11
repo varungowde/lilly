8335f9d6061f4a6a508cc202b5fb54621ce97169
vm1821 <vm1821@jagmail.southalabama.edu>
2019-05-11 01:42:19 -0500

    Updates

---
+++ precache-manifest.2cfbee1730b54887fd4f550fcb92a491.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e63e46d091d2cc648850",
+    "url": "/lilly/static/js/runtime~main.e63e46d0.js"
+  },
+  {
+    "revision": "901226c9c67a9b02c116",
+    "url": "/lilly/static/js/main.901226c9.chunk.js"
+  },
+  {
+    "revision": "036304812cbc90d7240d",
+    "url": "/lilly/static/js/2.03630481.chunk.js"
+  },
+  {
+    "revision": "901226c9c67a9b02c116",
+    "url": "/lilly/static/css/main.e983fbac.chunk.css"
+  },
+  {
+    "revision": "ce502175abd5a663da7311077a267d19",
+    "url": "/lilly/index.html"
+  }
+];
